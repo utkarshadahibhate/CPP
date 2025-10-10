07513cfe215abc415f5920f6f22e5d4cd22eb615
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-10 09:52:07 +0000

    10/10

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cout<<"Enter 1st number : ";
+    cin>>a;
+    cout<<"Enter 2nd number : ";
+    cin>>b;
+    c = a + b;
+    cout<<"Addition is :"<<c<<endl;
+}
