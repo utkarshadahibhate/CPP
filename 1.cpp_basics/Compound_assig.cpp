bd6fc0e8470b8613899d34c6614805d90e4af0e9
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-13 17:06:18 +0000

    compound_assignment

---
+++ 1.cpp_basics/Compound_assig.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 10;
+    int multi = 11;
+    int x = 5;
+    x+=1;
+    cout<<x<<endl;
+    multi *= x;
+    sum += x;
+    cout<<sum<<" "<<multi<<endl;
+}
