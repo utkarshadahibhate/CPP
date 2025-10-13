be439b474fd165d57d814758e7576bb623636633
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-13 12:43:56 +0000

    13/10

---
+++ Welcome.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"May i know your name : ";
+    getline(cin,name);
+    cout<<"Hello welcome to cpp "<<name<<endl;
+    return 0;
+
+}
