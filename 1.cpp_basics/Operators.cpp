29929e701750591c9263699a6c2225c5ac33a89c
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-14 13:44:20 +0000

    Operators

---
+++ 1.cpp_basics/Operators.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Arithmetic 
+    // +,-, *,/,%
+    // cout<<10+10<<endl;
+    // cout<<10-10<<endl;
+    // cout<<10*10<<endl;
+    // cout<<10/10<<endl;
+    // float res = (float)10/3;
+    // cout<<res<<endl;
+
+    // Relational Operators 
+    // <,>,<=,>=,==,!=
+    // cout<<(10>5)<<endl;
+    // cout<<(10<5)<<endl;
+    // cout<<(10<=10)<<endl;
+    // cout<<(10>=10)<<endl;
+    // cout<<(10==10)<<endl;
+    // cout<<(110!=10)<<endl;
+
+    // Logical Operators 
+    // &&,|| , !
+    // cout<<(10>5&&10<100)<<endl;
+    // cout<<(10<5&&10<100)<<endl;
+    // cout<<(10<5||10<100)<<endl;
+    // cout<<!(10<5||10<100)<<endl;
+    // cout<<!(0)<<endl;
+
+    // Assignment 
+    // =,+=,-=,*=,/=,...
+    // int p = 10;
+    // p=20;
+    // p=p+20;
+    // p+=20;
+    // p-=10;
+    // cout<<p;
+
+    // Bitwise Operators 
+    // &,|,^,~,<<,>>
+
+    cout<<(4&5)<<endl;
+    cout<<(4|5)<<endl;
+    cout<<(4^5)<<endl;
+    cout<<(10<<2)<<endl;
+    cout<<(10>>2)<<endl;
+    cout<<(~10)<<endl;
+    cout<<(~-10)<<endl;
+}
