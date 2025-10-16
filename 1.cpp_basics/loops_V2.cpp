40eaa5590609aefe0c2ac8e5905145dfd6f39255
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-16 14:43:03 +0000

    16/10/25

---
+++ 1.cpp_basics/loops_V2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+// Sum of n consecutive numbers using while loop
+int main(){
+    // int n, sum_con = 0;
+    // cout<<"Enter a number"<<endl;
+    // cin>>n;
+    // while(n>=0){
+    //     // cout<<n<<" ";
+    //     sum_con += n;
+    //     n--;
+    // }
+    // cout<<"\n"<<"The sum of the consecutive numbers : "<<sum_con<<endl;
+
+// Sum of n consecutive numbers using for loop
+
+// int sum_con1=0,user;
+// cout<<"Enter you fav number : "<<endl;
+// cin>>user;
+// for(int b=0; b<=user; b++){
+//     cout<<b<<" ";
+//     sum_con1 += b;
+// }
+// cout<<"\n"<<"Sum of the consecutive numbers : "<<sum_con1<<endl;
+
+
+// To print alphabets in matrix form
+char j='A';
+int i;
+
+while(i<=){
+    while(j<'E'){
+        cout<<j<<endl;
+        j++
+    }
+}
+
+}
+
+
+
