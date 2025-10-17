313dac6e7c1251ae95890a1bc0b8c903b2bef321
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-17 12:17:33 +0000

    code_2

--- Fibonacci_series.cpp
+++ Fibonacci_series.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 int main()
 {
-    int temp1=0, temp=1, nextterm;
+    int user,temp1=0, temp2=1, nextterm=0;
+    cout<<"Enter a number of terms for the series: "<<endl;
+    cin>>user;
 
-    if(temp1<=)
+    for(int i=1; i<=user; i++)
+    {
+        if(i == 1)
+        {
+            cout<<temp1<<",";
+            continue;
+        }
+        if(i == 2)
+        {
+            cout<<temp2<<",";
+            continue;
+        }
+        // updating the values
+        nextterm = temp1 + temp2;
+        // temp2 = nextterm; 
+        temp1 = temp2;
+        temp2 = nextterm;
+        cout<<nextterm<<",";
+    }
+    
 
 }
