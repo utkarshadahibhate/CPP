34b2917d806af2eb354c810fa583f1c2e89caeb5
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-18 14:05:12 +0000

    code_2

--- Programiz_Codes_Practice/HCF_GCD.cpp
+++ Programiz_Codes_Practice/HCF_GCD.cpp
@@ -10,4 +10,21 @@ int main(){
     int num1,num2,HCF;
     cout<<"Enter two number: ";
     cin>>num1>>num2;
+
+    if (num2>num1)
+    {// swapping of numbers
+        int temp = num2;
+        num2 = num1;
+        num1 = temp;    
+    }
+    for(int i=1; i<=num2; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            HCF = i;
+        }
+        
+    }
+    cout<<"HCF of given two number is: "<<HCF<<endl;
+    
 }
